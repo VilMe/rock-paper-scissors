f1c3123baadba199ec7fe1db1808eb1318be28ba
VilMe <vilma.medrano00@gmail.com>
2025-09-16 21:48:47 -0500

    define functions for class RPS

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ class RPS:          #creating a class because we need to define some state
         print('Welcome to Rock Paper Scissor - VilmaNation Style')
         
         self.moves: dict ={'rock': '🪨', 'paper': '📄', 'scissors': '✂️'}
+
+        self.valid_moves: list[str] = list(self.moves.keys())
+
+
+    def play_game(self):
+        ...
+
+    def display_move(self):
+        ...
+
+    def check_move(self):
+        ...
